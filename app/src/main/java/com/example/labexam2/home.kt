18a4c2979d871a7bd5c7a6503c698234ff3b78da
punsara222 <thurali283@gmail.com>
2025-08-24 02:15:13 +0530

    mad labExam 2 initial commit2

--- app/src/main/java/com/example/labexam2/home.kt
+++ app/src/main/java/com/example/labexam2/home.kt
@@ -5,28 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-import android.widget.TextView
 
-
-class MainActivity : AppCompatActivity() {
+class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        //navigate to signup
-        val signupTextView = findViewById<TextView>(R.id.textView7)
-        signupTextView.setOnClickListener {
-            val intent = Intent(this, signup::class.java)
-            startActivity(intent)
-        }
-
     }
 }
--- app/src/main/java/com/example/labexam2/signup.kt
+++ app/src/main/java/com/example/labexam2/signup.kt
@@ -1,6 +1,8 @@
 package com.example.labexam2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class signup : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //navigate to login
+        val signupTextView = findViewById<TextView>(R.id.textView20)
+        signupTextView.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
